622bcadbf186964361bf4c32cfb61804db3a9721
Votre Nom <chebihi.ilias@gmail.com>
2018-12-02 20:00:34 +0100

    modification semaine

--- fich_semaine_derniere.sh
+++ fich_semaine_derniere.sh
@@ -6,3 +6,6 @@ read -p "Quel répertoire vous intéresse aujourd'hui ?" rep
 cd $rep
 echo "Voilà ce qu'on a dans $rep :"
 ls
+echo "les fichiers modifiés il y a une semaine sont :"
+echo$(find -type f -mtime -7)
+
